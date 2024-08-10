f2204e0606bf8ea4c448dd52d3686aaac2083b68
Kaygizzle <62054578+KG-khangelani@users.noreply.github.com>
2024-08-10 22:09:14 +0200

    Question 4 header update

--- question4.cpp
+++ question4.cpp
@@ -3,3 +3,112 @@
 //
 
 #include "question4.h"
+
+using namespace std;
+
+Date::Date() {
+    theday = 14;
+    themonth = 8;
+    theyear = 1752;
+};
+
+Date::Date(int day, int month, int year) {
+    theday = day;
+    themonth = month;
+    theyear = year;
+};
+
+int Date::getDay() const {
+    return theday;
+};
+
+int Date::getMonth() const {
+    return themonth;
+};
+
+int Date::getYear() const {
+    return theyear;
+};
+
+void Date::setDay(int day) {
+    theday = day;
+};
+
+void Date::setMonth(int month) {
+    themonth = month;
+};
+
+void Date::setYear(int year) {
+    theyear = year;
+};
+
+
+
+int Date::monthLength() const {
+    if (themonth == 2) {
+        if (theyear % 4 == 0) {
+            if (theyear % 100 == 0) {
+                if (theyear % 400 == 0) {
+                    return 29;
+                }
+                return 28;
+            }
+            return 29;
+        }
+        return 28;
+    }
+    if (themonth == 4 || themonth == 6 || themonth == 9 || themonth == 11) {
+        return 30;
+    }
+    return 31;
+};
+
+Date &Date::operator++() {
+    theday++;
+    if (theday > monthLength()) {
+        theday = 1;
+        themonth++;
+        if (themonth > 12) {
+            themonth = 1;
+            theyear++;
+        }
+    }
+    return *this;
+};
+
+Date &Date::operator--() {
+    theday--;
+    if (theday < 1) {
+        themonth--;
+        if (themonth < 1) {
+            themonth = 12;
+            theyear--;
+        }
+        theday = monthLength();
+    }
+    return *this;
+};
+
+bool Date::operator<(const Date &d) const {
+    if (theyear < d.theyear) {
+        return true;
+    } else if (theyear > d.theyear) {
+        return false;
+    } else {
+        if (themonth < d.themonth) {
+            return true;
+        } else if (themonth > d.themonth) {
+            return false;
+        } else {
+            if (theday < d.theday) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+};
+
+void Date::operator<<(ostream &os) const {
+    os << theday << "/" << themonth << "/" << theyear;
+};
--- question4.h
+++ question4.h
@@ -13,83 +13,21 @@ using namespace std;
 class Date{
     public:
          //constructors
-        Date() {
-            theday = 14;
-            themonth = 8;
-            theyear = 1752;
-         }
-        Date(int day, int month, int year) {
-            theday = day;
-            themonth = month;
-            theyear = year;
-        }
+        Date();
+        Date(int day, int month, int year);
         //accessors
-        int getDay() const {
-            return this->theday;
-        }
-        int getMonth() const {
-            return this->themonth;
-        }
-        int getYear() const {
-            return this->theyear;
-        }
+        int getDay() const;
+        int getMonth() const;
+        int getYear() const;
         //mutators
-        void setDay(int day) {
-            this->theday = day;
-        }
-        void setMonth(int month) {
-            this->themonth = month;
-        }
-        void setYear(int year) {
-            this->theyear = year;
-        }
+        void setDay(int day);
+        void setMonth(int month);
+        void setYear(int year);
         //operators to calculate next and previous days
-        Date &operator++() {
-            theday++;
-            if (theday > monthLength()) {
-                theday = 1;
-                themonth++;
-                if (themonth > 12) {
-                    themonth = 1;
-                    theyear++;
-                }
-            }
-            return *this;
-        }
-        Date &operator--() {
-            theday--;
-            if (theday < 1) {
-                themonth--;
-                if (themonth < 1) {
-                    themonth = 12;
-                    theyear--;
-                }
-                theday = monthLength();
-            }
-            return *this;
-        }
-        bool operator<(const Date &d) const {
-            if (theyear < d.theyear) {
-                return true;
-            } else if (theyear > d.theyear) {
-                return false;
-            } else {
-                if (themonth < d.themonth) {
-                    return true;
-                } else if (themonth > d.themonth) {
-                    return false;
-                } else {
-                    if (theday < d.theday) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            }
-        }
-        void operator<<(ostream &os) const {
-            os << theday << "/" << themonth << "/" << theyear;
-        }
+        Date &operator++();
+        Date &operator--();
+        bool operator<(const Date &d) const;
+        void operator<<(ostream &os) const;
 
     private:
         //the current day month and year
@@ -98,27 +36,7 @@ class Date{
         int theyear;
         //return the length of current month, taking into
         //account leap years
-        int monthLength() const {
-            if (themonth == 2) {
-                if (theyear % 4 == 0) {
-                    if (theyear % 100 == 0) {
-                        if (theyear % 400 == 0) {
-                            return 29;
-                        } else {
-                            return 28;
-                        }
-                    } else {
-                        return 29;
-                    }
-                } else {
-                    return 28;
-                }
-            } else if (themonth == 4 || themonth == 6 || themonth == 9 || themonth == 11) {
-                return 30;
-            } else {
-                return 31;
-            }
-        }
+        int monthLength() const;
 };
 
 #endif //ASS3_QUESTION4_H
