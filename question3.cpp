36d0f0793f27d7955615d6d1f735b4aae0ad2c78
Kaygizzle <62054578+KG-khangelani@users.noreply.github.com>
2024-08-11 23:53:00 +0200

    Question 3 answer

--- question3.cpp
+++ question3.cpp
@@ -9,8 +9,31 @@ using namespace std;
 class PersonType
 {
     public:
-        PersonType();
-        PersonType(string n, int id, string bd);
+        PersonType() {
+            name = "";
+            ID = 0;
+            birthday = "";
+        }
+        PersonType(const string &n, const int &id, const string &bd) {
+            name = n;
+            ID = id;
+            birthday = bd;
+        }
+        friend bool operator==(const PersonType &person, const PersonType &person2) {
+            if (person.name == person2.name && person.ID == person2.ID && person.birthday == person2.birthday)
+                return true;
+            else
+                return false;
+        }
+        string getName() const {
+            return this->name;
+        }
+        int getID() const {
+            return this->ID;
+        }
+        string getBirthday() const {
+            return this->birthday;
+        }
     private:
         string name;
         int ID;
@@ -22,9 +45,9 @@ int main()
     PersonType family[20], newBaby("Anny Dube", 20180912, "2 Sept");
 
     //Assume family has been initialised
-    for (int i = 0; i < 20; i++)
-        if (family.birthday[5] == newBaby.birthday)
-            cout << family.name[5] << " share a birthday with "
-                 <<newBaby.name;
+    for (int i = 0; i < 20; i++) // NOLINT(*-loop-convert)
+        if (family[i].getBirthday() == newBaby.getBirthday())
+            cout << family[i].getName() << " share a birthday with "
+                 <<newBaby.getName();
     return 0;
 }
